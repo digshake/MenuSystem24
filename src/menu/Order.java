5839eccb165b97fd4ee4e6cbdad51deb9092570e
Doug Shook <dshook@wustl.edu>
2024-03-04 10:48:15 -0600

    feature complete!

--- src/menu/Order.java
+++ src/menu/Order.java
@@ -4,6 +4,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Order {
+	
+	private final double TAX_RATE = 0.1;
 
 	//instance variables
 	private List<MenuItem> items;
@@ -20,11 +22,27 @@ public class Order {
 		return items.size();
 	}
 	
-	public double computeTotal() {
+	public double computeSubtotal() {
 		double sum = 0;
 		for(MenuItem mi : items) {
 			sum += mi.getPrice();
 		}
 		return sum;
+	}
+	
+	public double computeTax() {
+		return computeSubtotal() * TAX_RATE;
+	}
+	
+	public double computeGrandTotal() {
+		return computeSubtotal() + computeTax();
+	}
+	
+	public void removeItem(MenuItem mi) {
+		items.remove(mi);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello!");
 	}
 }
